5e42f39263ff9f1bd075408543b61805a53583b5
rafaeltonini <rafaeltonini@gmail.com>
2021-07-16 17:30:47 -0300

    Updates

---
+++ precache-manifest.f18c4eaf96a5bc4cd28ed1c78f3a2d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f99b7195881ee6682a19f8f2c77ffb3",
+    "url": "/rafood/index.html"
+  },
+  {
+    "revision": "0e49b7972d40fd521672",
+    "url": "/rafood/static/css/main.7608451e.chunk.css"
+  },
+  {
+    "revision": "09f6ab8dbbdc97fecf33",
+    "url": "/rafood/static/js/2.f5065d16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rafood/static/js/2.f5065d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e49b7972d40fd521672",
+    "url": "/rafood/static/js/main.82b6ea73.chunk.js"
+  },
+  {
+    "revision": "173680fe494916d32712",
+    "url": "/rafood/static/js/runtime-main.733bbfec.js"
+  }
+]);
